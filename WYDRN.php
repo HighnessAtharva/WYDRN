04cca01def0a607d2069a28329b334a038a2835b
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2022-03-08 16:46:57 +0530

    SQL insert statements added.

--- WYDRN.php
+++ WYDRN.php
@@ -8,9 +8,25 @@ div{
 </style>
 ';
 
+	include("connection.php");
+    $user_data = check_login($con);
+    $username=$user_data['user_name'];
+
+    //global variables initializing to ''
+    $videogame='';
+    $platform='';
+    $album='';
+    $artist='';
+    $book='';
+    $author='';
+    $movie='';
+    $movierelease='';
+    $TV='';
+    $streamplatform='';
 
     //video game validation
     if ((!empty($_POST['Videogame'])) &&  (!empty($_POST['Platform']))){
+        global $videogame, $platform;
         $videogame=$_POST['Videogame'];
         $platform=$_POST['Platform']; 
 
@@ -20,6 +36,7 @@ div{
 
     //music validation
     if ((!empty($_POST['Album'])) &&  (!empty($_POST['Artist']))){
+        global $album, $artist; 
         $album=$_POST['Album'];
         $artist=$_POST['Artist'];
 
@@ -29,6 +46,7 @@ div{
 
     //book validation
     if ((!empty($_POST['Book'])) &&  (!empty($_POST['Author']))){
+        global $book, $author;  
         $book=$_POST['Book'];
         $book=trim($book,".");
         $author=$_POST['Author'];
@@ -39,6 +57,7 @@ div{
 
     //movie validation
     if ((!empty($_POST['Movie'])) &&  (!empty($_POST['MovieRelease']))){
+        global $movie, $movierelease; 
         $movie=$_POST['Movie'];
         $movierelease=$_POST['MovieRelease'];
 
@@ -48,13 +67,31 @@ div{
 
     //tv validation
     if ((!empty($_POST['TV'])) &&  (!empty($_POST['StreamPlatform']))){
+        global $TV, $streamplatform; 
         $TV=$_POST['TV'];
         $streamplatform=$_POST['StreamPlatform'];
         if ((!empty($TV)) && (!empty($streamplatform)))
             $binging="<div> &#128250 Binging <b>".$TV."</b> on ".$streamplatform."</div>";
     }
 
+    // insert the fields into the database if at least one of the above fields is filled.
+    if ((!empty($videogame)) || (!empty($album)) || (!empty($book)) || (!empty($movie)) || (!empty($TV))) 
+    {
+        $sql="INSERT INTO `data`(`username`, `videogame`, `platform`, `album`, `artist`, `book`, `author`, `movie`, `year`, `tv`, `streaming`) 
+        VALUES ('$username', '$videogame', '$platform', '$album', '$artist', '$book', '$author', '$movie', '$movierelease', '$TV', '$streamplatform')";
+        
+        $result=mysqli_query($con,$sql);
 
+        // Check if insert statement was successful and display the relevant message    
+        if($result){
+            echo "The record has been inserted successfully successfully!<br>";
+        }
+        else{
+            echo "Record not inserted. ERROR -> ". mysqli_error($con);
+        }
+    }   
+
+   //this is stuff used to display the text on the browser. This will be eventually replaced by SQL queries. 
     if (!empty($playing)) echo $playing."<BR>";
     if (!empty($listening)) echo $listening."<BR>";
     if (!empty($reading)) echo $reading."<BR>";
@@ -63,4 +100,7 @@ div{
 
     if ((empty($playing))&& (empty($listening))&& (empty($reading))&& (empty($watching))&& (empty($binging)))
         echo "<div> &#128542 Nothing to see here.</div>";
+
+
+    
     ?>
