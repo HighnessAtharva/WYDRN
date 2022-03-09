73daa3b19153d6140c57998b534fc7ace69701d6
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2022-03-09 18:46:28 +0530

    FPDF Plugin added and added raw PDF export feature

---
+++ pdf.php
@@ -0,0 +1,31 @@
+<?php
+require('FPDF/mysql_table.php');
+include ('connection.php');
+
+class PDF extends PDF_MySQL_Table
+{
+function Header()
+{
+    // Title
+    $this->SetFont('Arial','',18);
+    $this->Cell(0,6,'Data Export',0,1,'C');
+    $this->Ln(10);
+    // Ensure table header is printed
+    parent::Header();
+}
+}
+
+$pdf = new PDF();
+$pdf->AddPage();
+// First table: output all columns
+$sql = "SELECT datetime,
+        CONCAT_WS(' - ', videogame, platform) AS VIDEOGAME,
+        CONCAT_WS(' - ', album, artist) AS ALBUM,
+        CONCAT_WS(' - ', book, author) AS BOOK,
+        CONCAT_WS(' - ', movie, year) AS MOVIE,
+        CONCAT_WS(' - ', tv, streaming) AS TV
+        from data where username='HighnessAtharva'";
+        
+$pdf->Table($con,$sql);
+$pdf->Output();
+?>
