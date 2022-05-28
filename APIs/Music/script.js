8423c03e2766fe9213b5e6179d6c741dd6983014
Atharva Shah <68660002+HighnessAtharva@users.noreply.github.com>
2022-05-28 22:59:12 +0530

    Update script.js

--- APIs/Music/script.js
+++ APIs/Music/script.js
@@ -12,11 +12,11 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadAlbums(searchTerm) {
-    const URL = `https://api.discogs.com/database/search?q=${searchTerm}&format=album&type=master&key=GbtsdCNjHakVzCoxtiCA&secret=tOdlsCemqLtJdEIJxIxOGsLRmyeJlbSQ`;
+    const URL = `https://ws.audioscrobbler.com/2.0/?method=album.search&album=${searchTerm}&limit=3&api_key=6a4eb1d0536cfe3583784a65332ee179&format=json`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    var results = data['results']
-        //console.log(results);
+    var results = data['results']['albummatches']['album'];
+
     if (data) displayAlbumList(results);
 }
 
@@ -34,10 +34,12 @@ function displayAlbumList(albums) {
     searchList.innerHTML = "";
     for (let idx = 0; idx < albums.length; idx++) {
         let albumListItem = document.createElement('div');
-        albumListItem.dataset.id = albums[idx]['id']; // setting movie id in  data-id
+        //albumListItem.dataset.id = albums[idx]['id']; // setting movie id in  data-id
+        albumListItem.dataset.name = albums[idx]['name'];
+        albumListItem.dataset.artist = albums[idx]['artist'];
         albumListItem.classList.add('search-list-item');
-        if (albums[idx]['thumb'] != null)
-            albumPoster = albums[idx]['thumb'];
+        if (albums[idx]['image'][1]["#text"] != null)
+            albumPoster = albums[idx]['image'][1]["#text"];
         else
             albumPoster = "https://i.ibb.co/hRCvsdq/image-not-found.png";
         albumListItem.innerHTML = `
@@ -45,8 +47,8 @@ function displayAlbumList(albums) {
             <img src = "${albumPoster}">
         </div>
         <div class = "search-item-info">
-            <h3>${albums[idx]['title']}</h3>
-            <p>${albums[idx]['year']}</p>
+            <h3>${albums[idx]['name']}</h3>
+            <p>${albums[idx]['artist']}</p>
         </div>`;
         searchList.appendChild(albumListItem);
     }
@@ -60,10 +62,11 @@ function loadalbumDetails() {
             // console.log(album.dataset.id);
             searchList.classList.add('hide-search-list');
             albumSearchBox.value = "";
-            const result = await fetch(`https://api.discogs.com/masters/${album.dataset.id}`);
+            //https: //ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6a4eb1d0536cfe3583784a65332ee179&artist=${album.dataset.artist}&album=${album.dataset.name}&format=json
+            const result = await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6a4eb1d0536cfe3583784a65332ee179&artist=${album.dataset.artist}&album=${album.dataset.name}&format=json`);
             const albumDetails = await result.json();
             //console.log(albumDetails);
-            displayalbumDetails(albumDetails);
+            displayalbumDetails(albumDetails['album']);
         });
     });
 }
@@ -72,16 +75,31 @@ function displayalbumDetails(details) {
     // need to figure out a way to add images here!
     resultGrid.innerHTML = `
     <div class = "movie-poster">
-        <img src = "${(details['thumb'] != null) ?  details['thumb'] : "https://i.ibb.co/hRCvsdq/image-not-found.png"}" alt = "album poster">
+        <img src = "${(details['image'][4]['#text'] != null) ?  details['image'][4]['#text'] : "https://i.ibb.co/hRCvsdq/image-not-found.png"}" alt = "album poster">
     </div>
     <div class = "movie-info">
-        <h3 class = "movie-title">${details['title']} - ${details['artists'][0]['name']}</h3>
+        <h3 class = "movie-title">${details['name']} - ${details['artist']}</h3>
+        
         <ul class = "movie-misc-info">
             <li class = "year">Release Date: ${details['year']}</li>
         </ul>
-        <p class = "genre"><b>Genre:</b> ${details['genres']}</p>
-    </div>
-    `;
+        
+        <p class = "genre"><b>Genre:</b> ${details['tags']['tag'][0]['name']}</p>
+        
+        <br>Wiki:<p>${details['wiki']['summary']}</p>
+        
+        <br><br><h4> Track Listing </h4> <br>
+        
+        <p class = "movie-misc-info>
+        `;
+    for (let idx = 0; idx < details['tracks']['track'].length; idx++) {
+        // resultGrid.innerHTML += "<li>";
+        resultGrid.innerHTML += details['tracks']['track'][idx]['name'];
+        resultGrid.innerHTML += "<br>"
+            // resultGrid.innerHTML += "</li>";
+    }
+
+    resultGrid.innerHTML += "</p></div>";
 }
 
 
