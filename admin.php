629c71b2f4d4fa7e6d190419dd077c154550b850
Anay Deshpande <anaydesh1234@gmail.com>
2022-08-05 19:58:45 +0530

    Style added

--- admin.php
+++ admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * All The adminstrative functions such as member count, member deletion and member data/authorization will be displayed here.
  *
@@ -8,7 +9,7 @@
  */
 
 error_reporting(E_ERROR | E_PARSE);
-if (!isset($_GET['letmein'])){
+if (!isset($_GET['letmein'])) {
   echo ("<img src='images/website/imposter.jpg' style='width: 100%; height: 100%;'>");
   die();
 }
@@ -17,19 +18,20 @@ require "connection.php";
 require "functions.php";
 
 // executes the query and returns the first row of the result set. 
-function executeSQL($con,$sql){
+function executeSQL($con, $sql)
+{
   if ($query = mysqli_query($con, $sql)) {
     $row = mysqli_fetch_array($query);
     return $row[0];
-    }else{
-        echo mysqli_error($con);
-    }
+  } else {
+    echo mysqli_error($con);
+  }
 }
 
 /*************
   TOTAL MEDIA COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT sum(allcount) AS Total_Count FROM(
+ *************/
+$sql = "SELECT sum(allcount) AS Total_Count FROM(
     (SELECT count(`videogame`) as allcount FROM `data` WHERE videogame!='')
     UNION ALL
     (SELECT count(album) AS allcount FROM `data` WHERE album!='')
@@ -41,142 +43,142 @@ $sql="SELECT sum(allcount) AS Total_Count FROM(
     (SELECT count(tv) AS allcount FROM `data` WHERE tv!='')
 )t";
 
-$total_media_count= executeSQL($con, $sql);
+$total_media_count = executeSQL($con, $sql);
 
 /*************
   TOTAL CURRENT ACTIVE USERS ON SITE
-*************/
-$sql="SELECT count(`user_name`) FROM `users` where `active`=1";
-$current_active_users= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`user_name`) FROM `users` where `active`=1";
+$current_active_users = executeSQL($con, $sql);
 
 /*************
   TOTAL USERS COUNT (TOTAL REGISTERED USERS)
-*************/
+ *************/
 
-$sql="SELECT count(`user_name`) FROM `users`";
-$total_users_count= executeSQL($con, $sql);
+$sql = "SELECT count(`user_name`) FROM `users`";
+$total_users_count = executeSQL($con, $sql);
 
 /*************
   TOTAL BOOKS COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT count(`book`) FROM `data` where `book`!=''";
-$total_book_count= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`book`) FROM `data` where `book`!=''";
+$total_book_count = executeSQL($con, $sql);
 
 /*************
   TOTAL MOVIE COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT count(`movie`) FROM `data` where `movie`!=''";
-$total_movie_count= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`movie`) FROM `data` where `movie`!=''";
+$total_movie_count = executeSQL($con, $sql);
 
 /*************
   TOTAL TV COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT count(`tv`) FROM `data` where `tv`!=''";
-$total_tv_count= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`tv`) FROM `data` where `tv`!=''";
+$total_tv_count = executeSQL($con, $sql);
 
 /*************
   TOTAL VIDEOGAME COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT count(`videogame`) FROM `data` where `videogame`!=''";
-$total_videogame_count= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`videogame`) FROM `data` where `videogame`!=''";
+$total_videogame_count = executeSQL($con, $sql);
 
 /*************
   TOTAL ALBUM COUNT (SUM OF ALL USERS)
-*************/
-$sql="SELECT count(`album`) FROM `data` where `album`!=''";
-$total_album_count= executeSQL($con, $sql);
+ *************/
+$sql = "SELECT count(`album`) FROM `data` where `album`!=''";
+$total_album_count = executeSQL($con, $sql);
 
 
 /*************
   TOP 50 MOST LOGGED BOOKS -> ACROSS ALL USERS
-*************/
-$sql="SELECT book, count(book) FROM `data` where book!='' GROUP BY book HAVING count(book)>1 ORDER BY count(book) DESC LIMIT 50";
+ *************/
+$sql = "SELECT book, count(book) FROM `data` where book!='' GROUP BY book HAVING count(book)>1 ORDER BY count(book) DESC LIMIT 50";
 
-$top_50_books=executeSQL($con, $sql);
+$top_50_books = executeSQL($con, $sql);
 
 
 /*************
   TOP 50 MOST LOGGED ALBUMS -> ACROSS ALL USERS
-*************/
-$sql="SELECT album, count(album) FROM `data` where album!='' GROUP BY album HAVING count(album)>1 ORDER BY count(album) DESC LIMIT 50";
+ *************/
+$sql = "SELECT album, count(album) FROM `data` where album!='' GROUP BY album HAVING count(album)>1 ORDER BY count(album) DESC LIMIT 50";
 
-$top_50_albums=executeSQL($con, $sql);
+$top_50_albums = executeSQL($con, $sql);
 
 
 /*************
   TOP 50 MOST LOGGED TV SHOWS -> ACROSS ALL USERS
-*************/
-$sql="SELECT tv, count(tv) FROM `data` where tv!='' GROUP BY tv HAVING count(tv)>1 ORDER BY count(tv) DESC LIMIT 50";
+ *************/
+$sql = "SELECT tv, count(tv) FROM `data` where tv!='' GROUP BY tv HAVING count(tv)>1 ORDER BY count(tv) DESC LIMIT 50";
 
-$top_50_tvs=executeSQL($con, $sql);
+$top_50_tvs = executeSQL($con, $sql);
 
 
 /*************
   TOP 50 MOST LOGGED VIDEOGAMES -> ACROSS ALL USERS
-*************/
-$sql="SELECT videogame, count(videogame) FROM `data` where videogame!='' GROUP BY videogame HAVING count(videogame)>1 ORDER BY count(videogame) DESC LIMIT 50";
+ *************/
+$sql = "SELECT videogame, count(videogame) FROM `data` where videogame!='' GROUP BY videogame HAVING count(videogame)>1 ORDER BY count(videogame) DESC LIMIT 50";
 
-$top_50_videogames=executeSQL($con, $sql);
+$top_50_videogames = executeSQL($con, $sql);
 
 /*************
   TOP 50 MOST LOGGED MOVIES -> ACROSS ALL USERS
-*************/
-$sql="SELECT movie, count(movie) FROM `data` where movie!='' GROUP BY movie HAVING count(movie)>1 ORDER BY count(movie) DESC LIMIT 50";
+ *************/
+$sql = "SELECT movie, count(movie) FROM `data` where movie!='' GROUP BY movie HAVING count(movie)>1 ORDER BY count(movie) DESC LIMIT 50";
 
-$top_50_movies=executeSQL($con, $sql);
+$top_50_movies = executeSQL($con, $sql);
 
 
 /*************
   GET TOP 50 MOST POPULAR USERS (USERS WITH MOST FOLLOWERS)
-*************/
+ *************/
 
-$sql="SELECT followed_username as popular_users, COUNT(*) AS follower_count 
+$sql = "SELECT followed_username as popular_users, COUNT(*) AS follower_count 
 FROM social
 GROUP BY followed_username ORDER BY follower_count desc limit 50";
-$popular_users= executeSQL($con, $sql);
+$popular_users = executeSQL($con, $sql);
 
 
 /*************
  GET USERS WHO ARE NOT VERIFIED (CHECK `USERS` TABLE)
-*************/
+ *************/
 
-$sql="SELECT COUNT(`user_name`) FROM users where verified =0";
-$not_verified= executeSQL($con, $sql);
+$sql = "SELECT COUNT(`user_name`) FROM users where verified =0";
+$not_verified = executeSQL($con, $sql);
 
 
 /*************
   GET USERS WHO HAVE NOT LOGGED ANY MEDIA IN THE LAST 6 MONTHS
-*************/
+ *************/
 
 
 
 /*************
   GET USERS WHO HAVE LOGGED MORE THAN 1000 MEDIA ITEMS (THIS CAN BE EXTENDED LATER TO CHECK FOR SPAMMING)
-*************/
+ *************/
 
 
 
 /*************
   TOTAL MEDIA ADDED LAST 3 MONTHS (ACROSS ALL USERS)
-*************/
+ *************/
 
 
 
 /*************
   TOTAL MEDIA ADDED LAST 6 MONTHS
-*************/
+ *************/
 
 
 
 /*************
   TOTAL MEDIA ADDED LAST YEAR
-*************/
+ *************/
 
 
 
 /*************
   AVERAGE MEDIA ADDED PER DAY (ACROSS ALL USERS) -> USEFUL TO DETERMINE RATE AT WHICH DB SIZE IS INCREASING
-*************/
+ *************/
 
 
 ?>
@@ -186,46 +188,138 @@ $not_verified= executeSQL($con, $sql);
 ******************* -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <title>WYDRN - Admin Dashboard</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="CSS/admin.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>WYDRN - Admin Dashboard</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="CSS/admin.css">
 </head>
 
-<body >
+<body>
 
-    <div class="container">
-    
-        <div class="stat-item"> TOTAL MEDIA ADDED BY ALL USERS: <span> <?php echo($total_media_count) ?> </span></div>
-        <div class="stat-item">CURRENT ACTIVE USERS ON SITE: <span> <?php echo($current_active_users) ?></span></div>
- 
-        <!-- Add more stat items below in the same format as above. -->
-        <div class="stat-item">TOTAL USERS COUNT: <span> <?php echo "$total_users_count";?> </span></div>
-        <div class="stat-item">TOTAL BOOKS ADDED BY ALL USERS: <span> <?php echo "$total_book_count;"?> </span></div>
-        <div class="stat-item"> TOTAL MOVIES ADDED BY ALL USERS: <span> <?php echo "$total_movie_count;" ?> </span></div>
-        <div class="stat-item">TOTAL TVS ADDED BY ALL USERS:  <span> <?php echo "$total_tv_count;" ?> </span></div>
-        <div class="stat-item">TOTAL VIDEOGAMES ADDED BY ALL USERS: <span> <?php echo "$total_videogame_count";?> </span></div>
-        <div class="stat-item">TOTAL ALBUMS ADDED BY ALL USERS:  <span> <?php echo "$total_album_count";?> </span></div>
-        <div class="stat-item">TOP 50 MOST LOGGED BOOKS: <span> <?php echo "$top_50_books";?> </span></div>
-        <div class="stat-item">TOP 50 MOST LOGGED ALBUMS: <span> <?php echo "$top_50_albums";?> </span></div>
-        <div class="stat-item">TOP 50 MOST LOGGED TV SHOWS: <span> <?php echo "$top_50_tvs";?> </span></div>
-        <div class="stat-item">TOP 50 MOST LOGGED VIDEOGAMES: <span> <?php echo "$top_50_videogames";?> </span></div>
-        <div class="stat-item">TOP 50 MOST LOGGED MOVIES: <span> <?php echo "$top_50_movies";?> </span></div>
-        <div class="stat-item">TOP 50 MOST POPULAR USERS (USERS WITH MOST FOLLOWERS): <span> <?php echo "$popular_users";?> </span></div>
-        <div class="stat-item">COUNT OF USERS WHO ARE NOT VERIFIED: <span> <?php echo $not_verified;?> </span></div>
-        <div class="stat-item">USERS WHO HAVE NOT LOGGED ANY MEDIA IN THE LAST 6 MONTHS: <span> <?php ?> </span></div>
-        <div class="stat-item">USERS WHO HAVE LOGGED MORE THAN 1000 MEDIA ITEMS: <span> <?php ?> </span></div>
-        <div class="stat-item"> TOTAL MEDIA ADDED LAST 3 MONTHS: <span> <?php ?> </span></div>
-        <div class="stat-item"> TOTAL MEDIA ADDED LAST 6 MONTHS: <span> <?php ?> </span></div>
-        <div class="stat-item">TOTAL MEDIA ADDED LAST YEAR: <span> <?php ?> </span></div>
-        <div class="stat-item">AVERAGE MEDIA ADDED PER DAY (ACROSS ALL USERS): <span> <?php ?> </span></div>
+  <div class="table-wrapper">
+    <table class="fl-table">
+      <thead>
+        <tr>
+          <th colspan="2" class="heading2">Admin</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>TOTAL MEDIA ADDED BY ALL USERS</td>
+          <td><?php echo ($total_media_count) ?></td>
+        </tr>
+        
+        <tr>
+          <td> CURRENT ACTIVE USERS ON SITE </td>
+          <td> <?php echo ($current_active_users) ?> </td>
+        </tr>   
 
-    </div>
+        <tr>
+          <td> TOTAL USERS COUNT </td>
+          <td> <?php echo "$total_users_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOTAL BOOKS ADDED BY ALL USERS </td>
+          <td> <?php echo "$total_book_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOTAL MOVIES ADDED BY ALL USERS </td>
+          <td> <?php echo "$total_movie_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOTAL TVS ADDED BY ALL USERS </td>
+          <td> <?php echo "$total_tv_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOTAL VIDEOGAMES ADDED BY ALL USERS </td>
+          <td> <?php echo "$total_videogame_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOTAL ALBUMS ADDED BY ALL USERS </td>
+          <td> <?php echo "$total_album_count" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST LOGGED BOOKS </td>
+          <td> <?php echo "$top_50_books" ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST LOGGED ALBUMS </td>
+          <td> <?php echo "$top_50_albums"; ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST LOGGED TV SHOWS </td>
+          <td> <?php echo "$top_50_tvs"; ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST LOGGED VIDEOGAMES </td>
+          <td> <?php echo "$top_50_videogames"; ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST LOGGED MOVIES </td>
+          <td>  <?php echo "$top_50_movies"; ?> </td>
+        </tr>
+
+        <tr>
+          <td> TOP 50 MOST POPULAR USERS (USERS WITH MOST FOLLOWERS) </td>
+          <td> <?php echo "$popular_users"; ?> </td>
+        </tr>
+
+        <tr>
+          <td> COUNT OF USERS WHO ARE NOT VERIFIED </td>
+          <td> <?php echo $not_verified; ?> </td>
+        </tr>
+
+        <tr>
+          <td> USERS WHO HAVE NOT LOGGED ANY MEDIA IN THE LAST 6 MONTHS </td>
+          <td> <?php ?> </td>
+        </tr>
+
+        <tr>
+          <td> USERS WHO HAVE LOGGED MORE THAN 1000 MEDIA ITEMS </td>
+          <td> <?php ?> </td>
+        </tr>
+        
+        <tr>
+          <td> TOTAL MEDIA ADDED LAST 3 MONTHS </td>
+          <td> <?php ?> </td>
+        </tr>
+        
+        <tr>
+          <td> TOTAL MEDIA ADDED LAST 6 MONTHS </td>
+          <td> <?php ?> </td>
+        </tr>
+        
+        <tr>
+          <td> TOTAL MEDIA ADDED LAST YEAR </td>
+          <td> <?php ?> </td>
+        </tr>
+        
+        <tr>
+          <td> AVERAGE MEDIA ADDED PER DAY (ACROSS ALL USERS) </td>
+          <td> <?php ?> </td>
+        </tr>
+        
+      </tbody>
+    </table>
+  </div>
+
 </body>
+
 </html>
