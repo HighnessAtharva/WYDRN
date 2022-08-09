8c1cbcc35d0b5b42a0c3a5e84e5e3abf4499d032
HighnessAtharva <HighnessAtharva@gmail.com>
2022-08-09 23:54:45 +0530

    search_users.php significantly improved

--- search_users.php
+++ search_users.php
@@ -38,17 +38,115 @@ require "header.php";
 
 
 <body>
-<center>
-<br><br>
-<!-- Search box. -->
-   <input type="text" id="search" placeholder="Search for users" autocomplete="off"/>
 
-   <!-- Suggestions will be displayed in below div. -->
-   <div id="display">
+<!--title-->
+<div class="heading">
+  <h1>Search Users<span>Find friendlies with the best tastes.</span></h1>
+</div>
+
+<!----------------------------
+        FLEX COLUMNS
+------------------------------->
+<div class="columns">
+    
+    <!----------------------------
+        Top 10 Most Followed users
+    ------------------------------->
+    <div class="column" id="most-followed-div">
+        
+        <div class='most-followed-header-div'>
+            <h2> Top 10 <p>Most Followed Users</p></h2> 
+        </div>
+        
+        <div class='most-followed-items'>  
+            <?php
+            // Get the top 10 most followed users.
+            $sql="SELECT `followed_username` as `user_name`, COUNT(*) as `follower_count` from social GROUP BY `followed_username` ORDER BY `follower_count` desc  limit 8";
+            $query = mysqli_query($con, $sql);
+            if (mysqli_num_rows($query) > 0) {
+                for ($i = 1; $i <9; $i++) {
+                    $row[$i] = mysqli_fetch_array($query);
+                    $username = $row[$i]['user_name'];
+                    $follower_count = $row[$i]['follower_count'];
+            ?>
+                        <!-- HTML PART -->
+                        <a href=<?php echo "profile.php?user_name=".$username; ?>> 
+                            <div class='most-followed-person'>
+                                <big class='most-followed-username'><?php echo $username?></big>
+                                <br>
+                                <small class='most-followed-follower-count'><?php echo $follower_count; ?> Followers</small>
+                            </div>
+                        </a>
+                        
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+   
 
 
-   </div>
-</center>
+    <!----------------------------
+       Search Box
+    ------------------------------->
+
+
+    <div class="column" id='search-div'>
+            <center>
+            <input type="text" id="search" placeholder="Search for users" autocomplete="off"/>
+            <!-- Suggestions will be displayed in below div. -->
+            <div id="display"></div>
+            </center>
+    </div>
+ 
+
+
+
+    <!----------------------------
+    Top 10 Users with most logged items 
+    ------------------------------->
+    <div class="column" id="most-active-div">
+        <div class='most-active-header-div'>
+            <h2> Top 10 <p>Most Active Users</p></h2> 
+        </div>
+        <div class='most-active-items'>  
+            <?php
+            // Get the top 10 most followed users.
+            $sql="SELECT sum(allcount) as media_count, username  FROM(
+                (SELECT username, count(`videogame`) as allcount FROM `data` WHERE videogame!=''  GROUP BY username)
+                UNION ALL
+                (SELECT username, count(album) AS allcount FROM `data` WHERE album!=''  GROUP BY username)
+                UNION ALL
+                (SELECT username, count(book) AS allcount FROM `data` WHERE book!=''  GROUP BY username)
+                UNION ALL
+                (SELECT username, count(movie) AS allcount FROM `data` WHERE movie!='' GROUP BY username)
+                UNION ALL
+                (SELECT username, count(tv) AS allcount FROM `data` WHERE tv!=''  GROUP BY username)
+            )t group by username ORDER BY media_count DESC;";
+            $query = mysqli_query($con, $sql);
+            if (mysqli_num_rows($query) > 0) {
+                for ($i = 1; $i <9; $i++) {
+                    $row[$i] = mysqli_fetch_array($query);
+                    
+                    // $pfp=$row[$i]['profile_pic'];
+                    $username = $row[$i]['username'];
+                    $media_count = $row[$i]['media_count'];
+
+            ?>
+                        <a href=<?php echo "profile.php?user_name=".$username; ?>> 
+                            <div class='most-active-person'>
+                                <big class='most-active-username'><?php echo $username;?></big>
+                                <br>
+                                <small class='most-active-media-count'><?php echo $media_count;?> Medias</small>
+                            </div>
+                        </a>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
 
 
 <script>
