887b93c095f549550311ad2c48988bc57aef69b6
HighnessAtharva <HighnessAtharva@gmail.com>
2022-08-23 23:07:39 +0530

    admin page significantly improved

--- admin.php
+++ admin.php
@@ -10,8 +10,8 @@
 
 error_reporting(E_ERROR | E_PARSE);
 if (!isset($_GET['letmein'])) {
-    echo ("<img src='images/website/imposter.jpg' style='width: 100%; height: 100%;'>");
-    die();
+  echo ("<img src='images/website/imposter.jpg' style='width: 100%; height: 100%;'>");
+  die();
 }
 
 require "connection.php";
@@ -20,12 +20,12 @@ require "functions.php";
 // executes the query and returns the first row of the result set.
 function executeSQL($con, $sql)
 {
-    if ($query = mysqli_query($con, $sql)) {
-        $row = mysqli_fetch_array($query);
-        return $row[0];
-    } else {
-        echo mysqli_error($con);
-    }
+  if ($query = mysqli_query($con, $sql)) {
+    $row = mysqli_fetch_array($query);
+    return $row[0];
+  } else {
+    echo mysqli_error($con);
+  }
 }
 
 /*************
@@ -245,16 +245,42 @@ GET PROFILE PHOTO, USERNAME, AND EMAIL FOR EACH USER
 $sql = "SELECT `id`, `user_name`, `profile_pic`, `email` FROM users";
 $userlist = array();
 if ($query = mysqli_query($con, $sql)) {
-    while ($row = mysqli_fetch_assoc($query)) {
-        $uid = $row['id'];
-        $uname = $row["user_name"];
-        $pfp = $row["profile_pic"];
-        $emailadd = $row["email"];
-        array_push($userlist, array("uid" => $uid, "user_name" => $uname, "profile_pic" => $pfp, "email" => $emailadd));
-    }} else {
-    echo mysqli_error($con);
+  while ($row = mysqli_fetch_assoc($query)) {
+    $uid = $row['id'];
+    $uname = $row["user_name"];
+    $pfp = $row["profile_pic"];
+    $emailadd = $row["email"];
+    array_push($userlist, array("uid" => $uid, "user_name" => $uname, "profile_pic" => $pfp, "email" => $emailadd));
+  }
+} else {
+  echo mysqli_error($con);
 }
 
+
+$data = array(
+  array("Total Media Count", $total_media_count,"images/Icons/Book.svg"),
+  array("Users On Site Currently", $current_active_users,"images/Icons/Book.svg"),
+  array("Total Registered Users", $total_users_count,"images/Icons/Book.svg"),
+  array("Total Books Logged", $total_book_count,"images/Icons/Book.svg"),
+  array("Total Movies Logged", $total_movie_count,"images/Icons/Book.svg"),
+  array("Total TV Shows Logged", $total_tv_count,"images/Icons/Book.svg"),
+  array("Total Videogames Logged", $total_videogame_count,"images/Icons/Book.svg"),
+  array("Total Albums Logged", $total_album_count,"images/Icons/Book.svg"),
+  array("Unverified Users", $not_verified,"images/Icons/Book.svg"),
+  array("Inactive Users (6 months)", $dead_accounts,"images/Icons/Book.svg"),
+  array("Super Active Users", $whale_users,"images/Icons/Book.svg"),
+  array("Logged Items (Yesterday)", $total_media_added_yesterday,"images/Icons/Book.svg"),
+  array("Logged Items (Last Week)", $total_media_added_last_week,"images/Icons/Book.svg"),
+  array("Logged Items (Last Month)", $total_media_added_last_month,"images/Icons/Book.svg"),
+  array("Logged Items (Last Year)", $total_media_added_last_year,"images/Icons/Book.svg"),
+  array("Average Media Per User", $avg_media_per_user,"images/Icons/Book.svg"),
+  array("Most Logged Book", $top_books,"images/Icons/Book.svg"),
+  array("Most Logged Album", $top_albums,"images/Icons/Book.svg"),
+  array("Most Logged TV Show", $top_tv,"images/Icons/Book.svg"),
+  array("Most Logged Videogame", $top_games,"images/Icons/Book.svg"),
+  array("Most Logged Movie", $top_movies,"images/Icons/Book.svg"),
+  array("Most followed user", $popular_users,"images/Icons/Book.svg"),
+);
 ?>
 
 <!-------------------------------------------------------------------------------------
@@ -277,128 +303,140 @@ if ($query = mysqli_query($con, $sql)) {
 
 <body>
 
-<!-- BUTTON GROUPS SERVING VARIETY OF FUNCTIONS -->
-<div class="text-center" id="btn-group"><br>
-  <button id='site-stats-btn' class='btn btn-primary'>Site Stats</button>
-  <button id='user-management-btn' class='btn btn-primary'>Manage Users</button>
-</div>
-
-
-<!-- SITEWIDE STATS BEGINS-->
-  <div id='site-stats-table' class="table-wrapper" style="display:none;">
-    <table class="fl-table">
-      <thead>
-        <tr>
-          <th colspan="2" class="heading2">Sitewide Stats</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr><td>TOTAL MEDIA ADDED BY ALL USERS</td><td><?php echo $total_media_count; ?></td></tr>
-        <tr><td> CURRENT ACTIVE USERS ON SITE </td><td> <?php echo $current_active_users; ?> </td></tr>
-        <tr><td> TOTAL USERS COUNT </td><td> <?php echo $total_users_count; ?> </td></tr>
-        <tr><td> TOTAL BOOKS ADDED BY ALL USERS </td><td> <?php echo $total_book_count; ?> </td></tr>
-        <tr><td> TOTAL MOVIES ADDED BY ALL USERS </td><td> <?php echo $total_movie_count; ?> </td></tr>
-        <tr><td> TOTAL TVS ADDED BY ALL USERS </td><td> <?php echo $total_tv_count; ?> </td></tr>
-        <tr><td> TOTAL VIDEOGAMES ADDED BY ALL USERS </td><td> <?php echo $total_videogame_count; ?> </td></tr>
-        <tr><td> TOTAL ALBUMS ADDED BY ALL USERS </td><td> <?php echo $total_album_count; ?> </td></tr>
-        <tr><td> COUNT OF USERS WHO ARE NOT VERIFIED </td><td> <?php echo $not_verified; ?> </td></tr>
-        <tr><td> COUNT OF USERS WHO HAVE NOT LOGGED ANY MEDIA </td><td> <?php echo $dead_accounts; ?> </td></tr>
-        <tr><td> COUNT OF USERS WITH MORE THAN 1000 MEDIA ITEMS </td><td> <?php echo $whale_users; ?> </td></tr>
-        <tr><td> TOTAL MEDIA ADDED YESTERDAY </td><td> <?php echo $total_media_added_yesterday; ?> </td></tr>
-        <tr><td> TOTAL MEDIA ADDED LAST WEEK </td><td> <?php echo $total_media_added_last_week; ?> </td></tr>
-        <tr><td> TOTAL MEDIA ADDED LAST MONTH </td><td> <?php echo $total_media_added_last_month; ?> </td></tr>
-        <tr><td> TOTAL MEDIA ADDED LAST YEAR </td><td> <?php echo $total_media_added_last_year; ?> </td></tr>
-        <tr><td>  AVERAGE MEDIA ADDED PER USER  </td><td> <?php echo $avg_media_per_user; ?> </td></tr>
-        <tr><td> MOST LOGGED BOOK </td><td> <?php echo $top_books; ?> </td></tr>
-        <tr><td> MOST LOGGED ALBUM </td><td> <?php echo $top_albums; ?> </td></tr>
-        <tr><td> MOST LOGGED TV SHOW </td><td> <?php echo $top_tv; ?> </td></tr>
-        <tr><td> MOST LOGGED VIDEOGAME </td><td> <?php echo $top_games; ?> </td></tr>
-        <tr><td> MOST LOGGED MOVIE </td><td>  <?php echo $top_movies; ?> </td></tr>
-        <tr><td> MOST POPULAR USER </td><td> <?php echo $popular_users; ?> </td></tr>
-      </tbody>
-    </table>
+  <!-- BUTTON GROUPS SERVING VARIETY OF FUNCTIONS -->
+  <div class="text-center" id="btn-group"><br>
+    <button id='site-stats-btn' class='btn btn-primary'>Site Stats</button>
+    <button id='user-management-btn' class='btn btn-primary'>Manage Users</button>
   </div>
-<!-- SITEWIDE STATS END -->
 
 
-
-<!-- USER MANAGEMENT TABLE BEGINS -->
-<div id="user-mgmt-table" style="display:block;">
-
-  <?php
-foreach ($userlist as $user) {
-    ?>
-    <div class='user-container'>
-    <img class='user-pfp' src=<?php echo $user['profile_pic'] ?> >
-    <div class='user-name'> <?php echo $user['user_name'] ?></div>
-    <div class='user-email'> <?php echo $user['email'] ?></div>
-        <div class='user-buttons'>
-
-        <!-- <?php echo "window.location.href='delete_user_confirm.php?user_name=" . $user['user_name'] . "'" ?> -->
-
-           <!-- View Profile Button -->
-            <button class='btn btn-success' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='profile.php?user_name=" . $user['user_name'] . "'" ?>
-            >View Profile
-            </button>
+  <!-- SITEWIDE STATS BEGINS-->
 
 
-            <!-- Delete User Button -->
-            <button class='btn btn-primary' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='stats.php?user_name=" . $user['user_name'] . "'" ?>
-            >View Stats
-            </button>
+  <div id='site-stats-table' style="display:none;">
+  
 
-            <!-- Delete User Button -->
-            <button class='btn btn-danger' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='delete_user_confirm.php?user_name=" . $user['user_name'] . "'" ?>
-            >Delete User
-            </button>
+  <div class="wrapper">
+        <div class="main-container">
 
+        <?php foreach($data as $row) {
+          ?> 
+          
+  
+            <!--STAT CARD-->
+            <div class="user-box first-box">
+                <div class="activity card" style="--delay: .2s">
+                    <div class="flex">
+                        <!--Stat Logo-->
+                        <div class="destination-profile flex-child-one">
+                            <img class="profile-img" src=<?php echo $row[2]?> alt="" />
+                        </div>
+
+                        <!--Stat Name and Desc-->
+                        <div class="flex-child-two">
+                            <div class="title"><?php echo $row[0]?></div>
+                        </div>
+                    </div>
+
+                    <div class="destination">
+                        <div class="destination-card">
+
+                            <!--Circle -->
+                            <div class="circle">
+                            
+                                <div class="counter"><?php echo $row[1]?></div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!--STAT CARD END-->
+
+            <?php
+        }
+        ?>
+
+            <!--Wrapper Stuff-->
         </div>
     </div>
 
+  <!-- SITEWIDE STATS END -->
+  </div>
+
+
+  <!-- USER MANAGEMENT TABLE BEGINS -->
+  <div id="user-mgmt-table" style="display:block;">
+
     <?php
-} //end of foreach
-?>
+    foreach ($userlist as $user) {
+    ?>
+      <div class='user-container'>
+        <img class='user-pfp' src=<?php echo $user['profile_pic'] ?>>
+        <div class='user-name'> <?php echo $user['user_name'] ?></div>
+        <div class='user-email'> <?php echo $user['email'] ?></div>
+        <div class='user-buttons'>
 
-</div>
-<!-- USER MANAGEMENT TABLE ENDS -->
+          <!-- <?php echo "window.location.href='delete_user_confirm.php?user_name=" . $user['user_name'] . "'" ?> -->
+
+          <!-- View Profile Button -->
+          <button class='btn btn-success' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='profile.php?user_name=" . $user['user_name'] . "'" ?>>View Profile
+          </button>
+
+
+          <!-- Delete User Button -->
+          <button class='btn btn-primary' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='stats.php?user_name=" . $user['user_name'] . "'" ?>>View Stats
+          </button>
+
+          <!-- Delete User Button -->
+          <button class='btn btn-danger' id=<?php echo $user['uid'] ?> onclick=<?php echo "window.location.href='delete_user_confirm.php?user_name=" . $user['user_name'] . "'" ?>>Delete User
+          </button>
+
+        </div>
+      </div>
+
+    <?php
+    } //end of foreach
+    ?>
+
+  </div>
+  <!-- USER MANAGEMENT TABLE ENDS -->
 
 
 
-<!-------------------------------------------------------------------------------------
+  <!-------------------------------------------------------------------------------------
 JAVASCRIPT 
 ------------------------------------------------------------------------------------->
-<script>
+  <script>
+    // TOGGLE VISIBILITY FOR BUTTON SITEWIDE STATS
+    const btnSiteStats = document.getElementById('site-stats-btn');
+    const siteStatsDiv = document.getElementById('site-stats-table');
+
+    btnSiteStats.onclick = function() {
+      if (siteStatsDiv.style.display !== "none") {
+        siteStatsDiv.style.display = "none";
+      } else {
+        //set other divs to none
+        userMgmtDiv.style.display = "none";
+        siteStatsDiv.style.display = "block";
+      }
+    };
 
 
-// TOGGLE VISIBILITY FOR BUTTON SITEWIDE STATS
-const btnSiteStats= document.getElementById('site-stats-btn');
-const siteStatsDiv= document.getElementById('site-stats-table');
-
-btnSiteStats.onclick = function () {
-if (siteStatsDiv.style.display !== "none") {
-  siteStatsDiv.style.display = "none";
-} else {
-  //set other divs to none
-  userMgmtDiv.style.display = "none";
-  siteStatsDiv.style.display = "block";
-}
-};
-
-
-// TOGGLE VISIBILITY FOR BUTTON USER MANAGEMENT
-const btnUserMgmt= document.getElementById('user-management-btn');
-const userMgmtDiv= document.getElementById('user-mgmt-table');
-btnUserMgmt.onclick = function () {
-  if (userMgmtDiv.style.display !== "none") {
-    userMgmtDiv.style.display = "none";
-  } else {
-    //set other divs to none
-    siteStatsDiv.style.display = "none";
-    userMgmtDiv.style.display = "block";
-  }
-};
-
-
-</script>
+    // TOGGLE VISIBILITY FOR BUTTON USER MANAGEMENT
+    const btnUserMgmt = document.getElementById('user-management-btn');
+    const userMgmtDiv = document.getElementById('user-mgmt-table');
+    btnUserMgmt.onclick = function() {
+      if (userMgmtDiv.style.display !== "none") {
+        userMgmtDiv.style.display = "none";
+      } else {
+        //set other divs to none
+        siteStatsDiv.style.display = "none";
+        userMgmtDiv.style.display = "block";
+      }
+    };
+  </script>
 </body>
+
 </html>
