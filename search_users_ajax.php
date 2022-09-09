68f00480fab924e41f946196435431187fe2b758
HighnessAtharva <HighnessAtharva@gmail.com>
2022-09-09 19:10:38 +0530

    user search box improved

--- search_users_ajax.php
+++ search_users_ajax.php
@@ -2,7 +2,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
 <!-- CSS Stylesheet -->
-<link rel="stylesheet" href="CSS/search_users_ajax.css">
+<link rel="stylesheet" href="CSS/search_users.css">
 
 <?php
 /**
@@ -30,14 +30,14 @@ if (isset($_POST['search'])) {
 
       <center>
          <!-- <div class='search-results'> -->
-            <div class="list" id="list">
+            <div id="list">
                <!-- Assigning searched result in "Search box" in "search.php" file. -->
                <?php
                if ($result["user_name"]) {
                ?>
 
                   
-                  <a class="list-item" href=profile.php?user_name=<?php echo $result['user_name'] ?>>
+                  <a class="my-list-item" href=profile.php?user_name=<?php echo $result['user_name'] ?>>
                      <div class="list-item__avatar">
                         <img src="<?php echo $result['profile_pic'] ?>">
                      </div>
